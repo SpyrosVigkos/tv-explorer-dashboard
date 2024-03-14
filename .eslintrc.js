b6bb770934b79002e6df14a05f82bdf257180a26
Spyros Vigkos <52461295+SpyrosVigkos@users.noreply.github.com>
2024-03-14 22:29:05 +0100

    tv-explorer-dashboard init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "airbnb-base",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    quotes: ["error", "double"],
+    // Override/add rules settings here, such as:
+    "vue/no-unused-vars": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    // ...
+  },
+};
